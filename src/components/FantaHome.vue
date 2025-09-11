29831950b9171abbc5bcc66463e5e9d87feaacdc
Manuel <logiudicemanuel01@gmail.com>
2025-09-12 00:41:56 +0200

    add new squadre section

--- src/components/FantaHome.vue
+++ src/components/FantaHome.vue
@@ -1,15 +1,34 @@
 <script setup lang="ts">
+import { Icon } from '@iconify/vue'
 </script>
 
 <template>
-  <div id="fanta-home" class="w-full h-[calc(100%-60px)]">
+  <div id="fanta-home" class="w-full h-[calc(100%-60px)] text-gray-700">
     <div class="w-full h-full flex flex-col justify-center items-center">
-      <p class="text-[52px] text-gray-700 mt-[-150px]">Ciao <b>MANUEL!</b></p>
-      <p class="text-[22px] pt-2 text-gray-700">Benvenuto nel tuo sistema di gestione dell'asta personale.</p>
+      <p class="text-[52px] mt-[-150px]">Ciao <b>MANUEL!</b></p>
+      <p class="text-[22px] pt-2">Benvenuto nel tuo sistema di gestione dell'asta personale.</p>
       <div class="flex items-center gap-4 pt-12">
-        <div class="h-[200px] w-[220px] bg-gradient-to-r from-white/10 to-white/30 backdrop-blur-lg border border-white/20 rounded">INFO 1</div>
-        <div class="h-[200px] w-[220px] bg-gradient-to-r from-white/10 to-white/30 backdrop-blur-lg border border-white/20 rounded">INFO 2</div>
-        <div class="h-[200px] w-[220px] bg-gradient-to-r from-white/10 to-white/30 backdrop-blur-lg border border-white/20 rounded">INFO 3</div>
+        <div class="h-[200px] w-[220px] px-3 bg-gradient-to-r from-white/10 to-white/30 backdrop-blur-lg border border-white/20 rounded flex flex-col justify-center items-center">
+          <div class="h-[50px] w-[50px] rounded bg-blue-300 flex justify-center items-center">
+            <Icon icon="mdi:settings" width="30" height="30" />
+          </div>
+          <p class="pt-[10px] text-[16px] font-semibold">Gestione avanzata</p>
+          <p class="pt-[10px] text-center text-[14px]">Gestisci titolari e ballottaggi per ogni squadra.</p>
+        </div>
+        <div class="h-[200px] w-[220px] px-3 bg-gradient-to-r from-white/10 to-white/30 backdrop-blur-lg border border-white/20 rounded flex flex-col justify-center items-center">
+          <div class="h-[50px] w-[50px] rounded bg-pink-300 flex justify-center items-center">
+            <Icon icon="mdi:broadcast" width="30" height="30" />
+          </div>
+          <p class="pt-[10px] text-[16px] font-semibold">Asta in tempo reale</p>
+          <p class="pt-[10px] text-center text-[14px]">Crea live la tua squadra e controlla cosa fanno gli altri.</p>
+        </div>
+        <div class="h-[200px] w-[220px] px-3 bg-gradient-to-r from-white/10 to-white/30 backdrop-blur-lg border border-white/20 rounded flex flex-col justify-center items-center">
+          <div class="h-[50px] w-[50px] rounded bg-purple-300 flex justify-center items-center">
+            <Icon icon="mdi:strategy" width="30" height="30" />
+          </div>
+          <p class="pt-[10px] text-[16px] font-semibold">Strategia vincente</p>
+          <p class="pt-[10px] text-center text-[14px]">Salvati le statistiche utili per ogni giocatore.</p>
+        </div>
       </div>
     </div>
   </div>
--- src/components/FantaTeams.vue
+++ src/components/FantaTeams.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { getSquadre } from '@/composable/useDirectus.ts'
 
+interface Squadra {
+  id: number
+  nome: string,
+  stemma: string,
+}
+
+const squadre = ref(<Squadra[]>([]))
+
+onMounted(async () =>{
+  squadre.value = await getSquadre()
+})
 </script>
 
 <template>
-  <div class="w-full h-[calc(100%-60px)]">CIAO TEAMS</div>
+  <div class="w-full h-[calc(100%-60px)] flex items-center justify-center text-gray-700">
+    <div class="shadow-lg bg-white rounded-lg w-[90%] h-[90%] flex flex-col items-center">
+      <h1 class="text-[32px] flex items-center justify-center pt-[10px] font-bold">SQUADRE SERIE A</h1>
+      <p class="text-center italic text-gray-500 h-[40px]">Per ogni squadra puoi gestire i titolari e i ballottaggi.<br> Inoltre per ogni giocatore hai la possibilita' di inserire delle fantastatische utili da consultare durante l'asta.</p>
+      <div class="gap-4 flex-wrap pt-[70px] flex items-center justify-center">
+        <div class="border border-gray-400 w-[300px] h-[80px] rounded p-[10px] flex items-center hover:bg-blue-50 cursor-pointer" v-for="sq in squadre" :key="sq.id">
+          {{ sq.nome }}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
